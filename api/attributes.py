a747014eb6ff1ad033c5ec575ed1024f8a2d2343
中島　亮三 <aki323buri2@gmail.com>
2020-06-23 20:59:33 +0900

    refactor

--- api/attributes.py
+++ api/attributes.py
@@ -1,26 +1,43 @@
 from utils import fullpath 
 import pandas as pd 
 import os 
-from entl import createEntryList
 
-database = 'crm'
-camel = 'CRM'
+# database = 'crm'
+# camel = 'CRM'
+
+root_dir = fullpath('../myobexo')
+entl_url = lambda camel: 'http://help.myob.com.au/exo/schemas/exo87/{}/entl.html'.format(camel)
+entd_url = lambda camel: 'http://help.myob.com.au/exo/schemas/exo87/{}/entd.html'.format(camel)
+
+
+def attributes_csv(database, name):
+  return root_dir / database / 'attributes' / '{}.csv'.format(name)  
+
+def firstRowIsColumn(df):
+  df.columns = df.iloc[0]
+  return df.reindex(df.index.drop(0))
+
+def generateCsv(df, filename):
+  os.makedirs(filename.parent, exist_ok=True)
+  df.to_csv(filename)
+
+def createEntryList(database, camel):
+
+  df = pd.read_html(entl_url(camel))[1]
+  df = firstRowIsColumn(df)
+  entl = df
+
+  filename = fullpath(root_dir, database, 'entl.csv')
+  os.makedirs(filename.parent, exist_ok=True)
+  entl.to_csv(filename)
+
+  return entl
 
 def createAttributes(database, camel):
-  entd_url = 'http://help.myob.com.au/exo/schemas/exo87/{}/entd.html'.format(camel)
 
-  root_dir = fullpath('myobexo', database)
-  
   entl = createEntryList(database, camel)
 
-  def attributes_csv(name):
-    return root_dir / 'attributes' / '{}.csv'.format(name)  
-
-  tables = pd.read_html(entd_url)
-
-  def firstRowIsColumn(df):
-    df.columns = df.iloc[0]
-    return df.reindex(df.index.drop(0))
+  tables = pd.read_html(entd_url(camel))
 
   tables = [
     firstRowIsColumn(df) for df in tables 
@@ -30,11 +47,7 @@ def createAttributes(database, camel):
     df for df in tables if 'Column name' in df.columns
   ]
 
-  def genCsv(df, filenmae):
-    os.makedirs(filename.parent, exist_ok=True)
-    df.to_csv(filename)
-
   for i, name in enumerate(entl.Name):
     df = tables[i]
-    filename = fullpath(attributes_csv(name))
-    genCsv(df, filename)
+    filename = fullpath(attributes_csv(database, name))
+    generateCsv(df, filename)
--- api/main.py
+++ api/main.py
@@ -3,4 +3,15 @@ import pandas as pd
 
 from attributes import createAttributes 
 
-createAttributes('jobCosting', 'JobCosting')
+databases = [
+  ['creditors'  , 'Creditors'  ], 
+  ['debtors'    , 'Debtors'    ], 
+  ['stock'      , 'Stock'      ], 
+  ['crm'        , 'CRM'        ], 
+  ['jobCosting' , 'JobCosting' ], 
+]
+
+for database, camel in databases:
+  print('{} is in process...'.format(database))
+  createAttributes(database, camel)
+  print('{} is created!'.format(database))
